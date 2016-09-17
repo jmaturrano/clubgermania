6bfaa1a6230fac4de1defca46d67d6c485b5cbcf
mkcontreras <desarrollo@mksystemgroup.com>
2016-09-17 13:21:31 -0500

    Index modificado 98% y se agrego fuentes de icono

--- wp-content/themes/mksystem/parts/header-content.php
+++ wp-content/themes/mksystem/parts/header-content.php
@@ -42,8 +42,8 @@
         <div class="s1_2">
             <p  class="text-right s2 icono">
                 <img src="<?php echo get_template_directory_child(); ?>/inc/img/bandera.jpg" />
-                <a target="_blank"href="http://facebook.com"><i class="icono glyphicon glyphicon-earphone"></i></a>
-                <a target="_blank"href="#"><i class="icono fa fa-facebook oficial"></i></a>
+                <a target="_blank"href="#"><i class="fa fa-phone icono" style="margin-top: 1px;"></i></a>
+                <a target="_blank"href="#"><i class="icono fa fa-facebook-official"></i></a>
                 <a href="mailto:#"><i class="icono fa fa-envelope fa-social"></i></a>
             </p>
         </div>
